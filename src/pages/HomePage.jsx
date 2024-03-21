4cfd1f5218d7f890480c1204a3715caa032ac299
imshenza <itsshenza@gmail.com>
2024-03-21 13:03:21 +0530

    home page done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -64,7 +64,7 @@ const HomePage = () => {
         <div className="posters-section">
           {/* Display posters here */}
           {dynamicPoster}
-           <Poster
+           {/* <Poster
             imageUrl={"/images/img3.png"}
             time="10:00 AM"
             venue="Venue 1"
@@ -91,7 +91,7 @@ const HomePage = () => {
         imageUrl={"/images/img3.png"}
         time="02:00 PM"
         venue="Venue 3"
-      />
+      /> */}
           {/* Add more posters as needed */}
         </div>
       </main>
